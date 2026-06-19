d458185bda3dd910b899b327f7c82a3994accce9
Adi Shrivastava <shrivadi0405@gmail.com>
2026-06-19 22:00:47 +0530

    Added automated seed in userseed

--- Backend/seed/userseed.js
+++ Backend/seed/userseed.js
@@ -1,4 +1,13 @@
 const {Pool}=require("./db");
 
 users=[]
+async function seedusers(){
+    for(let i=1; i<=1000; i++){
+        const user='User${i}';
+        const age=(Math.floor(Math.random*40)+18);
+        const cities=['Indore','Agra','Bangalore','Pune','Mumbai','Chennai','Delhi'];
+        const city=randomitem(cities);
+        await pool.query('INSERT INTO USERS (name,age,city)')
+    }
+}
 
